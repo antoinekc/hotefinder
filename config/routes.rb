5743ce27a0cd9b81ec6fceb7b5ba8f33cad50856
antoinekc <kimkimkimkim0719@gmail.com>
2024-06-05 16:12:54 +0200

    Index Users + devise registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :missions
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Ajoutez une route pour l'index des utilisateurs
+  resources :users, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
