0d92f470d48af5456ddcd40229852ccb7f5b0045
Nzoops <Ton adresse mail githubls>
2024-06-06 12:14:57 +0200

    1st version navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
@@ -8,21 +8,30 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
-    colors: {
-      'HF-main': '#6e61fe',
-      'HF-second': '#f58282',
-      'HF-light': '#f1f1f1',
-      'HF-dark': '#737373',
-    },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', 'Rubik', ...defaultTheme.fontFamily.sans],
+        serif: ['Lora', ...defaultTheme.fontFamily.serif]
       },
-    },
+      colors: { 
+        test: '#6e61fe',
+        HFmain: {
+          100: '#8f85fe',
+          200: '#6e61fe',
+          300: '#6e61fe'
+        },
+        HFsecond: {
+          100: '#F7A1A1',
+          200: '#f58282'
+        },
+        HFlight: '#f1f1f1',
+        HFdark: '#737373'
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/container-queries')
   ]
-}
+};
