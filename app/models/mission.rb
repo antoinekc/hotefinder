2e2b00a58b564e5893c7bf208137a432dce482cb
antoinekc <kimkimkimkim0719@gmail.com>
2024-06-06 17:15:26 +0200

    selection du concierge via un drop down menu

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,9 +1,21 @@
 class Mission < ApplicationRecord
+
+  validate :start_date_cannot_be_in_the_past
+
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   belongs_to :host, class_name: "User", foreign_key: "host_id"
   has_many :missions_categories_join
   has_many :categories, through: :missions_categories_join
   belongs_to :city
+
+  private
+
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "Ne pas pas être antérieure à la date du jour.")
+    end
+  end
+
 end
 
 
