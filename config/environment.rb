72e416d58124b180e212e66a483c0676177ca88d
antoinekc <kimkimkimkim0719@gmail.com>
2024-06-11 11:15:57 +0200

    fixing smtp errors

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['MAILJET_API_KEY'],
+  :user_name => ENV['MAILJET_API_KEY'], #adding API Key
   :password => ENV['MAILJET_SECRET_KEY'],
   :domain => 'https://hotefinder-3980cefcc9bb.herokuapp.com',
   :address => 'in-v3.mailjet.com',
