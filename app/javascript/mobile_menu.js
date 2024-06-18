f54cc1f977f035a8ce205a9983cca0ffba0f3e10
antoinekc <kimkimkimkim0719@gmail.com>
2024-06-18 16:39:21 +0200

    fixing details

--- app/javascript/mobile_menu.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('resize', function() {
-  var mobileMenu = document.getElementById('mobile-menu');
-  if (window.innerWidth >= 768 && !mobileMenu.classList.contains('hidden')) {
-    mobileMenu.classList.add('hidden');
-  }
-});
