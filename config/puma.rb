f4efe26c89648390d63a554be76825124f45232b
valentin-orrit <vorrit@gmail.com>
2024-06-25 11:26:08 +0200

    again

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,7 @@ preload_app! if worker_count > 1
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-# port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "production" }
